d4b837d5015b4a604cd59677b5a00efd114eb93f
harrison7 <harrison@thecaspers.net>
2024-02-22 20:40:06 -0700

    Begin logic of purchasing upgrades

---
+++ play.js
@@ -0,0 +1,27 @@
+class Vial {
+
+}
+
+class Upgrade {
+    constructor() {
+        this.purchases = 0;
+        this.price = 10;
+    }
+
+    purchase() {
+        purchases++;
+        price *= 10;
+    }
+}
+
+class Game {
+    elements;
+    constructor() {
+        this.elements = [0, 0, 0, 0, 0, 0, 0, 0];
+        this.buttons = new Map();
+    }
+
+    pressButton(button) {
+        
+    }
+}
