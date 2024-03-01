099b4052b803bfb4a7f873a6d4fc6c5370efe77b
harrison7 <harrison@thecaspers.net>
2024-02-29 22:49:04 -0700

    Begin game creation

--- play.js
+++ play.js
@@ -1,49 +1,68 @@
-class Flask {
+// class Flask {
 
+// }
+
+// class Upgrade {
+//     constructor() {
+//         this.purchases = 0;
+//         this.price = 10;
+//     }
+
+//     purchase() {
+//         this.purchases++;
+//         this.price *= 10;
+//     }
+// }
+
+
+// class Game {
+//     elements;
+//     constructor() {
+//         this.elements = [0, 0, 0, 0, 0, 0, 0, 0];
+//     }
+
+//     pressButton(buttonType) {
+//         const button = this.buttons.get(buttonType);
+
+//         if (button) {
+//             alert(`${buttonType} button clicked!`);
+//         } else {
+//             console.error(`Button with type ${buttonType} not found.`);
+//         }
+//     }
+
+//     addButton(buttonType, buttonId) {
+//         const buttonElement = document.getElementById(buttonId);
+
+//         if (buttonElement) {
+//             this.buttons.set(buttonType, buttonElement);
+
+//             buttonElement.addEventListener('click', () => {
+//                 this.pressButton("flask");
+//             });
+//         } else {
+//             console.error(`Button element with id ${buttonId} not found.`);
+//         }
+//     }
+// }
+
+// const game = new Game();
+
+let clicks = 0;
+let elements = [0, 0, 0, 0, 0, 0, 0, 0];
+let upgrades = [0, 0, 0, 0, 0, 0, 0];
+
+function create() {
+    clicks += 1;
+    updateElements()
 }
 
-class Upgrade {
-    constructor() {
-        this.purchases = 0;
-        this.price = 10;
+function updateElements() {
+    elements[0] += (2 ** upgrades[0]);
+    if (clicks % 8 == 0) {
+        elements[1] += (2 ** upgrades[1]);
     }
 
-    purchase() {
-        purchases++;
-        price *= 10;
-    }
+    document.getElementById('hydrogen').textContent = elements[0];
+    document.getElementById('helium').textContent = elements[1];
 }
-
-
-class Game {
-    elements;
-    constructor() {
-        this.elements = [0, 0, 0, 0, 0, 0, 0, 0];
-        this.buttons = new Map();
-    }
-
-    pressButton(buttonType) {
-        const button = this.buttons.get(buttonType);
-
-        if (button) {
-            button.addEventListener('click', () => {
-                // Call the logic you want to execute when the button is clicked
-                alert(`${buttonType} button clicked!`);
-            });
-        } else {
-            console.error(`Button with type ${buttonType} not found.`);
-        }
-    }
-
-    addButton(buttonType, buttonId) {
-        const buttonElement = document.getElementById(buttonId);
-
-        if (buttonElement) {
-            this.buttons.set(buttonType, buttonElement);
-        } else {
-            console.error(`Button element with id ${buttonId} not found.`);
-        }
-    }
-}
-
-const game = new Game();
