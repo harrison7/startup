c44458b5a57eff438c41289beac40b0d3d32be0c
harrison7 <harrison@thecaspers.net>
2024-03-02 21:40:03 -0700

    Created leaderboard logic

--- play.js
+++ play.js
@@ -54,6 +54,13 @@ let upgrades = [0, 0, 0, 0, 0, 0, 0];
 let upgradeCosts = [10, 10, 10, 10, 10, 10, 10];
 let alertIDs = ["Htxt", "Hetxt", "Litxt", "Betxt", "Btxt", "Ctxt", "Ntxt"];
 
+const scoresData = [
+    { name: "Billy", score: -7 },
+    { name: "Bob", score: -6 },
+    { name: "Joe", score: -5 },
+    { name: "Man", score: 3 },
+];
+
 function combine(nums) {
     let sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     return sum;
@@ -122,3 +129,49 @@ async function upgrade(index) {
         document.getElementById(alertIDs[index]).textContent = text;
     }
 }
+
+function renderLeaderboard() {
+    const sortedScores = scoresData.sort((a, b) => b.score - a.score);
+
+    const leaderboardList = document.getElementById("leaderboard-list");
+
+    leaderboardList.innerHTML = "";
+
+    sortedScores.forEach((score, index) => {
+        const listItem = document.createElement("li");
+        if (score.score === -7) {
+            listItem.innerHTML = `${score.name} <b class="element hydrogen">H</b> (3)`;
+        } else if (score.score === -6) {
+            listItem.innerHTML = `${score.name} <b class="element helium">He</b> (3)`;
+        } else if (score.score === -5) {
+            listItem.innerHTML = `${score.name} <b class="element lithium">Li</b> (3)`;
+        } else if (score.score === -4) {
+            listItem.innerHTML = `${score.name} <b class="element beryllium">Be</b> (3)`;
+        } else if (score.score === -3) {
+            listItem.innerHTML = `${score.name} <b class="element boron">B</b> (3)`;
+        } else if (score.score === -2) {
+            listItem.innerHTML = `${score.name} <b class="element carbon">C</b> (3)`;
+        } else if (score.score === -1) {
+            listItem.innerHTML = `${score.name} <b class="element nitrogen">N</b> (3)`;
+        } else {
+
+        }
+        
+        leaderboardList.appendChild(listItem);
+    });
+}
+
+renderLeaderboard();
+
+function updateScores(newScores) {
+    scoresData.push(...newScores);
+
+    renderLeaderboard();
+}
+
+const newScores = [
+    { name: "Player6", score: 1000 },
+    { name: "Player7", score: 850 },
+];
+
+updateScores(newScores);
