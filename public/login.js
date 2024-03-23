3648fd441991ce16be2c6a2991e9b50f18828ddd
harrison7 <harrison@thecaspers.net>
2024-03-22 19:37:37 -0600

    Add login logic endpoints to login.js

--- public/login.js
+++ public/login.js
@@ -98,3 +98,34 @@ function login() {
 
     window.location.href = "play.html";
 }
+
+async function login() {
+    loginOrCreate(`/api/auth/login`);
+}
+  
+async function register() {
+    loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+  
+    if (response.ok) {
+        localStorage.setItem('userName', userName);
+        window.location.href = 'play.html';
+    } else {
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
+    }
+}
