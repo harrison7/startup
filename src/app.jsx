1b384978a1122fcc64127c24b00fb0b94d5fdb42
harrison7 <harrison@thecaspers.net>
2024-04-15 20:52:08 -0600

    Added navigation bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Guide } from './guide/guide';
 
-export default function App() {
-  <div>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <div>
     <header>
       <h1>Elementary</h1>
+      <nav>
+        <NavLink className='nav-link' to=''>login</NavLink>
+        <NavLink className='nav-link' to='play'>play</NavLink>
+        <NavLink className='nav-link' to='guide'>guide</NavLink>
+      </nav>
     </header>
 
-    <main>App components go here</main>
+    <Routes>
+      <Route path='/' element={<Login />} exact />
+      <Route path='/play' element={<Play />} />
+      <Route path='/guide' element={<Guide />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
 
     <footer>
       <p>About the creator</p>
@@ -16,5 +32,9 @@ export default function App() {
       <div><a href="https://github.com/harrison7/startup">GitHub</a></div>
     </footer>
   </div>
+  </BrowserRouter>
+);
 
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
