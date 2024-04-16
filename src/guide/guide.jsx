6147b32fe9ff3db091089cd4a2f4d450eb6b8938
harrison7 <harrison@thecaspers.net>
2024-04-15 20:37:03 -0600

    Page stubs

---
+++ src/guide/guide.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Guide() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>guide displayed here</div>
+    </main>
+  );
+}
