7c1e03fa43858a27e688aa438f97335dc0df8742
harrison7 <harrison@thecaspers.net>
2024-04-15 20:31:14 -0600

    Add header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  <div>
+    <header>
+      <h1>Elementary</h1>
+    </header>
+
+    <main>App components go here</main>
+
+    <footer>
+      <p>About the creator</p>
+      <div><a href="https://www.linkedin.com/in/harrison-casper-a945b32ab/">Harrison Casper</a></div>
+      <div><a href="https://github.com/harrison7/startup">GitHub</a></div>
+    </footer>
+  </div>
+
 }
